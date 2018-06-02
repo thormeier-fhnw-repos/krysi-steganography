42f0a5d40b9a44a96dfc09fcddb1d24bcfde1a42
Pascal Thormeier <pascal.thormeier@liip.ch>
2018-06-02 18:27:19 +0200

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,14 @@
+/**
+ * Main class for steganography presentation demo
+ */
+public class main
+{
+    /**
+     * Main methods
+     * @param args Console args
+     */
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World!");
+    }
+}
